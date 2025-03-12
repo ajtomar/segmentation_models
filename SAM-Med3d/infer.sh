3fd3ede1228af87228a8e9b28dd0317dde999aed
Ajay Tomar <aib232071@aice009.head.cm.hpc.iitd.ac.in>
2025-03-13 00:36:56 +0530

    initial commit

---
+++ SAM-Med3d/infer.sh
@@ -0,0 +1,5 @@
+python inference.py --seed 2024\
+ -cp path_to_checkpoint_path_sam_med3d.pth \
+ -tdp path_to_test_data_path -nc 1 \
+ --output_dir path_to_output_dir \
+ --task_name infer_turbo  
---
+++ SAM-Med3d/val.sh
@@ -0,0 +1,5 @@
+python validation.py --seed 2023\
+ -vp ./results/vis_sam_med3d \
+ -cp ./work_dir/union_train/sam_model_dice_best.pth \
+ -tdp ./fold1_data/validation -nc 1 \
+ --save_name ./results/sam_med3d.py
